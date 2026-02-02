39827f4139ea4d69ac97caeddafcd44183dfb13b
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2026-02-02 20:45:11 -0300

    Update lista_users.sh

--- modulosxray/lista_users.sh
+++ modulosxray/lista_users.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# list_users.sh
+# lista_users.sh - Listagem de Usuários
+
 USER_DB="/opt/XrayTools/users.db"
 TITLE_BAR='\033[1;47;34m'
 RESET='\033[0m'
@@ -10,10 +11,11 @@ printf "%-15s | %-37s | %s\n" "USER" "UUID" "EXPIRA"
 echo "-----------------------------------------------------------------------"
 if [ -f "$USER_DB" ]; then
     while IFS='|' read -r nick uuid expiry; do
+        # Verifica se a linha não está vazia antes de imprimir
         [ -n "$nick" ] && printf "%-15s | %-37s | %s\n" "$nick" "$uuid" "$expiry"
     done < "$USER_DB"
 else
-    echo "Vazio."
+    echo "Nenhum usuário cadastrado."
 fi
 echo "-----------------------------------------------------------------------"
 read -rp "Enter para voltar..."
